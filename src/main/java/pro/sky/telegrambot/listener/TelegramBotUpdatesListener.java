314ca8e9d800bcda3de46dc4ef0f8f2c1b67b627
VGAliyev <v.aliev84@yandex.ru>
2024-03-16 19:19:42 +0400

    First message "Hello, World!"

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +31,10 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
             // Process your updates here
+            if (update.message().text().equals("/start")) {
+                SendMessage sendMessage = new SendMessage(update.message().chat().id(), "Hello, World!");
+                SendResponse response = telegramBot.execute(sendMessage);
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
